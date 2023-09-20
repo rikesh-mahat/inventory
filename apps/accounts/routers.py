5bc3ed3d742b6d6e7f2bdf7a6c3ad49e2478dc5f
rikeshmahat2060012 <np03cs4s210278@heraldcollege.edu.np>
2023-09-20 21:48:17 +0545

    updated user router and removed forgot password

--- apps/accounts/routers.py
+++ apps/accounts/routers.py
@@ -6,14 +6,13 @@ from apps.accounts.views import (
     SupplierViewSet,
     BillerViewSet,
     WarehouseViewSet,
-    ForgotPasswordView
 )
 
 router = DefaultRouter()
-router.register("users", UserViewSet, basename="users")
+router.register("user", UserViewSet, basename="users")
 router.register("customers", CustomerViewSet, basename="customers")
 router.register("suppliers", SupplierViewSet, basename="suppliers")
 router.register("billers", BillerViewSet, basename="billers")
 router.register("warehouse", WarehouseViewSet, basename="warehouse")
-router.register("forgot-password", ForgotPasswordView, basename="forgot-password")
-urlpatterns = router.urls
+
+
