679c164f9ec88f994436ef501c466820bd539e76
rikeshmahat2060012 <np03cs4s210278@heraldcollege.edu.np>
2023-09-21 07:49:44 +0545

    register customer, biller and supplier

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
-from apps.accounts.models import (User, OTP)
+from apps.accounts.models import (User, OTP, Customer, Supplier)
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['email', 'username', 'phone']
+    list_display = ['id', 'email', 'username', 'phone']
     
     
 @admin.register(OTP)
 class OTPAdmin(admin.ModelAdmin):
     list_display = ['otp', 'user']
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['id']
+    
+    
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ['id']
